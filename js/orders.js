45690c25630c9e8addd8635e101b8e032cb383b6
DusanNesic95 <dusannesic001@gmail.com>
2016-11-28 23:05:07 +0100

    Prikaz tabele i pretraga.

--- js/orders.js
+++ js/orders.js
@@ -1,7 +1,21 @@
 var app = angular.module('MyApp', []);
 app.controller('MyCtrl', function($scope, $http) {
-    $http.get("http://www.w3schools.com/angular/customers.php")
+	$scope.orders = [];
+	$scope.search = '';
+
+    $http.post("http://dusannesicdevelopment.sytes.net:8181/getadminlist")
     .then(function (response) {
-    	$scope.names = response.data.records;
+    	console.log(response.data);
+    	$scope.orders = response.data;
+
+    	for (var i = 0; i < $scope.orders.length; i++) {
+    		if ($scope.orders[i].orderStatus) {
+    			$scope.orders[i].orderStatus = 'Ditangguhkan'
+    		} else {
+    			$scope.orders[i].orderStatus = '';
+    		}
+
+    		console.log($scope.orders[i]);
+    	}
     });
 });
